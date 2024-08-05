1b33c01958f8530279d379a21f90938b5831560d
Matheus de Gondra <matheusgondra951@gmail.com>
2024-08-05 12:38:47 -0300

    testando modulo Item

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       cpf: "12345678911",
       age: 23,
       address: "Rua do Limoeiro, 35"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :pizza,
+      description: "Pizza de peperoni",
+      unity_price: Decimal.new("35.5"),
+      quantity: 1
     }
   end
 end
