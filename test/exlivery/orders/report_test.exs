e3ca7dd7acadf7ee51a2065ba182f06ba23c1e21
Matheus de Gondra <matheusgondra951@gmail.com>
2024-08-06 09:18:01 -0300

    testando geração de relatórios

---
+++ test/exlivery/orders/report_test.exs
@@ -0,0 +1,31 @@
+defmodule Exlivery.Orders.ReportTest do
+  use ExUnit.Case
+
+  import Exlivery.Factory
+
+  alias Exlivery.Orders.Report
+  alias Exlivery.Orders.Agent, as: OrderAgent
+
+  describe "create/1" do
+    test "creates the report file" do
+      OrderAgent.start_link(%{})
+
+      :order
+      |> build()
+      |> OrderAgent.save()
+
+      :order
+      |> build()
+      |> OrderAgent.save()
+
+      Report.create("report_test.csv")
+
+      response = File.read!("report_test.csv")
+
+      expected_response =
+        "12345678911,pizza,1,35.5japonesa,2,20.50,76.50\n12345678911,pizza,1,35.5japonesa,2,20.50,76.50\n"
+
+      assert response == expected_response
+    end
+  end
+end
